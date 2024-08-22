f119822d9d7e108662805c5fa41ca40a623d1bde
officialhassan216 <hassikhan845@gmail.com>
2024-08-22 10:50:01 +0500

    clinic website

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// console.log("Hello");
+
+
+// let age = 20 ;
+// let user = prompt("Enter your age ...");
+
+// if (user == age) {
+//     alert("good");
+// } else {
+//     alert("Your AGE IS NOT EQUAL");
+// }
